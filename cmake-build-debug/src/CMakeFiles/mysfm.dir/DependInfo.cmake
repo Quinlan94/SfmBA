b4f3c93345d1ea28ab1d984ad4eed404d9db9cdd
quinlan <xzky15@163.com>
2018-04-19 21:39:14 +0800

    first time

---
+++ cmake-build-debug/src/CMakeFiles/mysfm.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/quinlan/Learn/SfmBA/src/CalculateCameraMatrix.cpp" "/home/quinlan/Learn/SfmBA/cmake-build-debug/src/CMakeFiles/mysfm.dir/CalculateCameraMatrix.cpp.o"
+  "/home/quinlan/Learn/SfmBA/src/Common.cpp" "/home/quinlan/Learn/SfmBA/cmake-build-debug/src/CMakeFiles/mysfm.dir/Common.cpp.o"
+  "/home/quinlan/Learn/SfmBA/src/FeatureMatching.cpp" "/home/quinlan/Learn/SfmBA/cmake-build-debug/src/CMakeFiles/mysfm.dir/FeatureMatching.cpp.o"
+  "/home/quinlan/Learn/SfmBA/src/SaveXYZimages.cpp" "/home/quinlan/Learn/SfmBA/cmake-build-debug/src/CMakeFiles/mysfm.dir/SaveXYZimages.cpp.o"
+  "/home/quinlan/Learn/SfmBA/src/Triangulation.cpp" "/home/quinlan/Learn/SfmBA/cmake-build-debug/src/CMakeFiles/mysfm.dir/Triangulation.cpp.o"
+  "/home/quinlan/Learn/SfmBA/src/fed.cpp" "/home/quinlan/Learn/SfmBA/cmake-build-debug/src/CMakeFiles/mysfm.dir/fed.cpp.o"
+  "/home/quinlan/Learn/SfmBA/src/nldiffusion_functions.cpp" "/home/quinlan/Learn/SfmBA/cmake-build-debug/src/CMakeFiles/mysfm.dir/nldiffusion_functions.cpp.o"
+  "/home/quinlan/Learn/SfmBA/src/utils.cpp" "/home/quinlan/Learn/SfmBA/cmake-build-debug/src/CMakeFiles/mysfm.dir/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
